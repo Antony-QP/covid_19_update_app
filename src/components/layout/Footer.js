67e319e56af1582af08fba4bdc788a44f97b4212
Antony Parker <antonyqparker@gmail.com>
2020-10-22 10:00:16 +0100

    first commit

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+export const Footer = () => {
+  return (
+    <footer class="page-footer red darken-2">
+    <div class="container">
+      <div class="row">
+      © 2020 Covid-19 Updater
+      <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+      </div>
+    </div>
+  </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+export const Navbar = () => {
+  return (
+    <nav className= "red darken-2">
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo center">COVID-19 UPDATER</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Covid-19 News</a></li>
+        <li><a href="badges.html">Symptoms</a></li>
+        <li><a href="collapsible.html">Help</a></li>
+      </ul>
+    </div>
+  </nav>
+  )
+}
+
+export default Navbar
