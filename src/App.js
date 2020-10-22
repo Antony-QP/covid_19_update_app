cb4b9e3d33bc38f90ec435f0661c8f888770e886
Antony Parker <antonyqparker@gmail.com>
2020-10-22 11:32:45 +0100

    passing hardcoded state to country_item via props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, { Component} from 'react';
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
+import CountryItem from './components/countries/Country_item'
 import './App.css';
 
-function App() {
+class App extends Component {
+  state = {
+    country: "Spain",
+    code: "ES",
+    confirmed: 1046641,
+    recovered: 196958,
+    critical: 1930,
+    deaths: 34366,
+    latitude: 40.463667,
+    longitude: -3.74922,
+    lastChange: "2020-10-21T19:00:55+02:00",
+    lastUpdate: "2020-10-22T11:00:05+02:00"
+  }
+
+  render(){
   return (
 <div>
 <body>
     <Navbar/>
   <main>
 
-
+  <CountryItem data={this.state}/>
   </main>
-
     <Footer/>
-
   </body>
 </div>
   );
+}
 }
 
 export default App;
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,7 +5,7 @@ export const Navbar = () => {
   return (
     <nav className= "red darken-2">
     <div class="nav-wrapper">
-      <a href="#" class="brand-logo center">COVID-19 UPDATER</a>
+      <a href="#" class="brand-logo center"><i className="fas fa-globe"></i>COVID-19 UPDATER</a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
         <li><a href="sass.html">Covid-19 News</a></li>
         <li><a href="badges.html">Symptoms</a></li>
