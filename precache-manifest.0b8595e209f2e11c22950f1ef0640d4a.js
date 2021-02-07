f23bc5a5923f14a882563ff074f8d8b1da5a6bd9
Antony Parker <antonyqparker@gmail.com>
2021-02-07 17:28:49 +0100

    Updates

---
+++ precache-manifest.0b8595e209f2e11c22950f1ef0640d4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e94fd1a771615fbca148e4e6c331fe3e",
+    "url": "/covid_19_update_app/index.html"
+  },
+  {
+    "revision": "6cd59e9a84d7a3bee515",
+    "url": "/covid_19_update_app/static/css/main.9edda5ce.chunk.css"
+  },
+  {
+    "revision": "9bea16e0255b7cde4830",
+    "url": "/covid_19_update_app/static/js/2.263b41fb.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/covid_19_update_app/static/js/2.263b41fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cd59e9a84d7a3bee515",
+    "url": "/covid_19_update_app/static/js/main.85a75e00.chunk.js"
+  },
+  {
+    "revision": "5884b0b99e332966c9d2",
+    "url": "/covid_19_update_app/static/js/runtime-main.bc0f5a73.js"
+  },
+  {
+    "revision": "c95ab437dc939a9a4ae049c6c58ffeed",
+    "url": "/covid_19_update_app/static/media/cdc-w9KEokhajKw-unsplash.c95ab437.jpg"
+  }
+]);
--- src/components/pages/Home.js
+++
@@ -1,9 +0,0 @@
-import React, {Fragment} from 'react'
-import Search from'../countries/Search'
-import Country_Item from '../countries/Country_item'
-
-export const Home = () => {
-  return (
-
-  )
-}
